f03f039e4c2382615a7089bfbd2906fd79f1ba00
sarslancs <sarslancs@gmail.com>
2018-09-23 13:40:38 +0100

    simplify code, remove unused imports/functions

--- lib/biobank_utils.py
+++ lib/biobank_utils.py
@@ -36,25 +36,17 @@ CSV_PATH = '/vol/biobank/12579/brain/ukb8972_extracted.csv'
 # Data directory if not given
 DATA_DIR = '/vol/biobank/12579/brain/fmri'
 
-import os, random, pickle
-from functools import partial
+import os
 
 import csv
+
 import numpy as np
-import nibabel as nib
-import matplotlib.pyplot as plt
 
 from scipy.spatial import distance
-from sklearn.preprocessing import minmax_scale
-
-from scipy.io import loadmat
-from scipy.ndimage import zoom
-from scipy.ndimage.morphology import binary_fill_holes
 
 from graph_utils import distance_scipy_spatial, adjacency
        
         
-
 def get_ids(num_subjects=None, conn_tag='25751_2_0', data_dir=DATA_DIR):
     """
         num_subjects   : number of subject IDs to get
@@ -144,89 +136,6 @@ def square_all_vectors(vectors, indexing='column', d=55, remove_ids=None):
     return np.array(networks)
 
 
-def generate_train_val_test_IDs(subject_ids, labels, test_ratio=0.2, seed=SEED):
-    '''
-    Randomly pick IDs for train, test and validation subjects
-    '''
-    
-       
-    np.random.seed(seed)
-    random.seed(seed)
-    
-    val_ratio = 0.1
-    train_ratio = 1.0 - test_ratio - val_ratio
-    
-    
-    IDs = range(len(labels))
-    F_IDs = [IDs[i] for i, label in enumerate(labels) if label == FEMALE]
-    M_IDs = [IDs[i] for i, label in enumerate(labels) if label == MALE]
-    
-    Fs = np.reshape(random.sample(F_IDs, len(F_IDs)), len(F_IDs))
-    Ms = np.reshape(random.sample(M_IDs, len(M_IDs)), len(M_IDs))
-    
-    # Partition datasets
-    F_train = int(len(F_IDs) * train_ratio)
-    F_test = int(len(F_IDs) * test_ratio)
-    
-    M_train = int(len(M_IDs) * train_ratio)
-    M_test = int(len(M_IDs) * test_ratio)
-    
-    # Arrange train IDs in a way that two sequential IDs have different genders
-    train_IDs = np.full((F_train + M_train), -1)
-    F_IDs = Fs[0:F_train]
-    M_IDs = Ms[0:M_train]
-    for i in range(M_train):
-        train_IDs[i*2] = M_IDs[i]
-    train_IDs[train_IDs == -1] = F_IDs
-    
-
-    # No need to arrange these the same as train IDs
-    test_IDs = np.concatenate((Fs[F_train:F_train+F_test], 
-                               Ms[M_train:M_train+M_test]))
-    val_IDs = np.concatenate((Fs[F_train+F_test:], Ms[M_train+M_test:]))
-        
-    return train_IDs, test_IDs, val_IDs
-
-
-def compute_adjacency_and_save(conn_tag='25751_2_0', metric='correlation', 
-                               indexing='column', k=10, label_name='31-0.0',
-                               remove_ids=None, save=False):
-    '''
-    Compute adjacency for all subjects available and save
-    '''
-    
-    # conn_tag look-up
-    #25750-2.0	rfMRI full correlation matrix, dimension 25
-    #25751-2.0	rfMRI full correlation matrix, dimension 100
-    #25752-2.0	rfMRI partial correlation matrix, dimension 25
-    #25753-2.0	rfMRI partial correlation matrix, dimension 100
-    #25754-2.0	rfMRI component amplitudes, dimension 25
-    #25755-2.0	rfMRI component amplitudes, dimension 100
-    
-    dims = {'25750_2_0': 25, '25751_2_0': 100, 
-            '25752_2_0': 25, '25753_2_0': 100,
-            '25754_2_0': 25, '25755_2_0': 100}
-    
-    subject_ids = get_ids(conn_tag=conn_tag) 
-    _, subject_ids = get_subject_label(subject_ids, label_name=label_name)
-    vectors = load_all_vectors(subject_ids, tag=conn_tag)
-    
-    N = compute_graph_structure_for_networks(vectors, metric, indexing, k=k,
-                                             remove_ids=remove_ids)
-    
-    if save:
-        save_to = '/vol/medic02/users/sa1013/CNN_GRAPH/cnn_graph/codebase/data/biobank_gender/'        
-        save_name = save_to + 'extended_ica_' + str(dims[conn_tag]) + '_' + \
-                    conn_tag + '_' + indexing + '_k' + str(k)
-        if remove_ids is not None:
-            id_tag = ''.join(['_{}'.format(i) for i in remove_ids])
-            save_name = save_name + id_tag
-        with open(save_name + '_N.pkl', 'w') as f:
-            pickle.dump(N, f)
-    
-    return N
-    
-
 def vector_to_square(vector, d=55, idx=None):
     square_net_from_file = np.zeros((d, d))
     tr = np.triu(np.ones((d, d)), k=1)
@@ -272,90 +181,6 @@ def compute_graph_structure_for_networks(vectors, metric='correlation',
     A = adjacency(dist, idx).astype(np.float32)
 
     return A
-
-
-def get_all_templates(d=100):
-    '''get_all_templates
-    '''
-    parc_name = 'rfMRI_ICA_d' + str(d) + '.nii.gz'    
-    template = root + 'data/biobank_gender/UKBiobank_BrainImaging_GroupMeanTemplates/T1.nii.gz'
-    
-    # Indexing starts from 1
-    valid_ids = [a-1 for a in good_components[str(d)]]
-
-    t1_nii = nib.load(template)
-    ica_nii = nib.load(root + data_dir + parc_name)
-    ica_valid = ica_nii.get_data()[:,:,:,valid_ids]
-    return ica_valid, ica_nii, t1_nii
-
-def save_ICA_maps(ica_nii):
-    '''save_ICA_maps
-    '''
-    valid_ids = [a-1 for a in good_components[str(ica_nii.shape[-1])]]
-    ica = ica_nii.get_data()[:,:,:,valid_ids]
-    # Save ICA maps        
-    for i in range(ica.shape[-1]):
-        img = nib.Nifti1Image(ica[:,:,:,i], ica_nii.affine)
-        nib.save(img, root + 'tmp/ica_{}.nii.gz'.format(i+1))
-    
-def map_data_to_brain(data, name, d=100, res=2):
-    '''map_data_to_brain
-    '''
-    
-    # Load templates
-    ica, ica_nii, nii = get_all_templates(d)
-    if res == 1:    
-        accum_maps = np.zeros(ica.shape[:3])    
-    else:
-        accum_maps = np.zeros(nii.get_data().shape)        
-    for i in range(ica.shape[-1]):
-        ica_map = ica[:,:,:,i]
-        ica_map[ica_map < 0] = 0 # mask out negative values
-        rescaled_map = ica_map / np.max(ica_map) # scale remaining vals
-        rescaled_map *= data[i] # Populate with data[i] based on ica weights         
-        if res > 1: # Upsample
-            rescaled_map = zoom(rescaled_map, 2, order=1) # Reshape to original space           
-        accum_maps += rescaled_map
-        # Save individual maps
-        # img = nib.Nifti1Image(rescaled_map, nii.affine)
-        # nib.save(img, root + 'vis/' + name + '_comp_{}_{}mm.nii.gz'.format(i+1, res))
-    
-    img = nib.Nifti1Image(accum_maps, nii.affine)
-    nib.save(img, root + 'vis/' + name + '_map_{}mm.nii.gz'.format(res))
-    img = nib.Nifti1Image(accum_maps / np.max(accum_maps), nii.affine)
-    nib.save(img, root + 'vis/' + name + '_scaled_map_{}mm.nii.gz'.format(res))
-    
-def map_temporal_data_to_brain(data, name, d=100, res=2):
-    '''map_data_to_brain
-    '''
-    
-    # Load templates
-    ica, ica_nii, nii = get_all_templates(d)
-     
-    if res == 1:      
-        longi = np.zeros((ica.shape[0], ica.shape[1], 
-                          ica.shape[2], data.shape[0]))
-    else:
-        longi = np.zeros((nii.get_data().shape[0], nii.get_data().shape[1], 
-                          nii.get_data().shape[2], data.shape[0]))
-    for t in range(data.shape[0]):
-        if res == 1:    
-            accum_maps = np.zeros(ica.shape[:3])    
-        else:
-            accum_maps = np.zeros(nii.get_data().shape)    
-        for i in range(ica.shape[-1]):
-            ica_map = ica[:,:,:,i]
-            ica_map[ica_map < 0] = 0 # mask out negative values
-            rescaled_map = ica_map / np.max(ica_map) # scale remaining vals
-            rescaled_map *= data[t, i] # Populate with data[i] based on ica weights         
-            if res > 1: # Upsample
-                rescaled_map = zoom(rescaled_map, 2, order=1) # Reshape to original space           
-            accum_maps += rescaled_map
-        
-        longi[:,:,:,t] = accum_maps
-        
-    img = nib.Nifti1Image(longi, nii.affine)
-    nib.save(img, root + 'vis/' + name + '_{}mm.nii.gz'.format(res))
 
 
 
