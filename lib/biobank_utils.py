e97098be46031a7717abd91e25c81cb2cd52743f
sarslancs <sarslancs@gmail.com>
2018-09-24 14:40:42 +0100

    remove obsolote static variables

--- lib/biobank_utils.py
+++ lib/biobank_utils.py
@@ -11,22 +11,7 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 
-
-
-# Indexing starts from 1, required for visualising maps
-good_components = {'100': [2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17,
-                           18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-                           34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 46, 48, 49, 50, 52,
-                           53, 57, 58, 60, 63, 64, 93],
-        
-                    '25': [1,  2,  3,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,  
-                           18, 19, 20, 21, 22]
-                    }
-    
-# Gender codes
-MALE    = 1
-FEMALE  = 0
-
+   
 # Default seed if not given by
 SEED = 101
 
